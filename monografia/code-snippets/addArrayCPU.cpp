13fc9b957f9af8d1696bc7b9fb537ffde14863fb
vinicius7427@gmail.com <vinicius7427@gmail.com>
2024-04-14 03:16:18 -0300

    Incluir e testar código C++ serial exemplo
    Exemplo este que será usado na seção 2.3.2
    
    2.3 -> NVIDIA CUDA, 2.3.2 -> Exemplo de Implementação

---
+++ monografia/code-snippets/addArrayCPU.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+// Função que adiciona os elementos de dois vetores
+void add(int n, float *x, float *y){
+  for (int i = 0; i < n; i++)
+    y[i] += x[i];
+}
+
+int main(void){
+  int N = 1<<27; // Mais de 134mi elementos
+
+  float *x = new float[N];
+  float *y = new float[N];
+
+  // Inicializar vetores no host
+  for (int i = 0; i < N; i++) {
+    x[i] = 3.77f; y[i] = 3.23f;
+  }
+
+  // Rodar na CPU
+  add(N, x, y);
+
+  // Checar se há erros (todos valores devem ser  7.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-7.0f));
+  std::cout << "Max error: " << maxError << "\n";
+
+  // Liberar memória
+  delete [] x;
+  delete [] y;
+
+  return 0;
+}
