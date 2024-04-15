f639e532d23817a71a136aa130805774e99b18e2
vinicius7427@gmail.com <vinicius7427@gmail.com>
2024-04-14 21:04:06 -0300

    Implementar versão em CUDA v2 de addArray (C++)
    monografia/code-snippets/addArrayGPUv3.cu
    
    Também possui uma versão para benchmark automático: addArrayGPUv3_benchmark.cu

---
+++ monografia/code-snippets/addArrayGPUv3.cu
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+
+// Kernel que adiciona os elementos de dois vetores
+__global__
+void add(int n, float *x, float *y){
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] += x[i];
+}
+
+int main(void){
+  int N = 1<<28; // 268.435.456 elementos
+
+  float *x, *y;
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  for (int i = 0; i < N; i++) {
+    x[i] = 3.77f; y[i] = 3.23f;
+  }
+
+  int blockSize = 256;
+  int numBlocks = ceil((N + blockSize - 1) / blockSize);
+
+  for (int i = 0; i < 10; i++) {
+    add<<<numBlocks, blockSize>>>(N, x, y);
+    cudaDeviceSynchronize();
+  }
+
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-7.0f));
+  std::cout << "Max error: " << maxError << "\n";
+
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
+}
---
+++ monografia/code-snippets/addArrayGPUv3_benchmark.cu
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+
+#include <chrono>
+using namespace std::chrono;
+
+// Kernel que adiciona os elementos de dois vetores
+__global__
+void add(int n, float *x, float *y){
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] += x[i];
+}
+
+int main(void){
+  int N = 1<<28; // 268.435.456 elementos
+
+  float *x, *y;
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  for (int i = 0; i < N; i++) {
+    x[i] = 3.77f; y[i] = 3.23f;
+  }
+
+  int blockSize = 256;
+  int numBlocks = ceil((N + blockSize - 1) / blockSize);
+
+  int runs = 10;
+  auto startTime = high_resolution_clock::now();
+
+  for (int i = 0; i < runs; i++) {
+    add<<<numBlocks, blockSize>>>(N, x, y);
+    cudaDeviceSynchronize();
+  }
+
+  auto duration = duration_cast<milliseconds>(high_resolution_clock::now() - startTime);
+  std::cout << "Avg exec time: " << duration.count() / runs << " ms" << "\n";
+
+  float expectedSum = 3.23f + runs * 3.77f;
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i] - expectedSum));
+  std::cout << "Max error: " << maxError << "\n";
+
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
+}
